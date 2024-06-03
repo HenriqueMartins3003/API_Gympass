d5b6a057da05c2cd26b35117f6e1d5385b6ecdab
Henrique De Franca Martins <henrique.martins@ciahering.com.br>
2024-06-03 14:44:09 -0300

    Feat: add prisma relationships

--- src/fastify/app.ts
+++ src/fastify/app.ts
@@ -1,12 +1,18 @@
 import fastify from "fastify";
-import {PrismaClient} from '@prisma/client'
+import {z} from 'zod'
+
 
 export const app = fastify()
-const prisma = new PrismaClient()
 
-prisma.user.create({
-    data: {
-        name: '',
-        email: ''
-    }
+
+app.post("/users", (request, reply) => {
+    const registerBodySchema = z.object({
+        name: z.string(),
+        email: z.string().email(),
+        password: z.string().min(6),
+    })
+
+    const {name,email,password} = registerBodySchema.parse(request.body)
 })
+
+
